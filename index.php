1137450ca7c490d7ad01c634528195c16dd238c3
shz-cod <web.shahidul.alam@gmail.com>
2023-01-17 21:20:17 +0600

    v0.1.3(Why us section)

--- index.php
+++ index.php
@@ -21,6 +21,55 @@
                 </div>
             </div>
         </section>
+
+        <section class="why_us d-block d-lg-flex gap-4 align-items-center">
+            <div class="master_card">
+            <div class="card">
+                    <h2 class="text-center">WHY US?</h2>
+                    <div class="card_body">
+                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut et quibusdam officiis in illum ex odio nulla quisquam dolorum ipsum cupiditate molestias praesentium iure totam, minima a. Fugiat illo doloremque consectetur possimus nihil fuga corrupti suscipit rem accusantium? Nobis, iure perferendis! Voluptatum necessitatibus provident repellat animi optio hic placeat assumenda.
+                        </p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Learn More</a>
+                </div>
+            </div>
+            <div class="cards d-xl-flex gap-2 align-items-center">
+                <div class="card">
+                    <div class="card_body">
+                        <div class="card_icon">
+                            <i class='bx bxs-star'></i>
+                        </div>
+                        <h4>Lorem ipsum dolor sit.</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card_body">
+                        <div class="card_icon">
+                            <i class='bx bxs-cube'></i>
+                        </div>
+                        <h4>Lorem ipsum dolor sit.</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card_body">
+                        <div class="card_icon">
+                            <i class='bx bxs-graduation'></i>
+                        </div>
+                        <h4>Lorem ipsum dolor sit.</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     </main>
 
     <?php include("./components/_footer.php"); ?>
